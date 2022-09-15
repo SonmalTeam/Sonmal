80acdc6bc23eb12cc21bd6774ef8b7934a059aed
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-09-15 17:19:07 +0900

    feat: Spring Boot 기본 세팅 | 프로젝트 생성 [S07P22D202-140]
    Former-commit-id: ca8ca4a0656dad405822da2c68e071acdb0a27d5

---
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/AssembleServerApplication.java
@@ -0,0 +1,13 @@
+package com.d202.assemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleServerApplication.class, args);
+	}
+
+}
