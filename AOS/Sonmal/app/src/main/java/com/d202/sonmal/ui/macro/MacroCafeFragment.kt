88b685b7c23c56a5fdb854a943ef3fe720a9ad8d
woway2488 <woway2488@naver.com>
2022-09-19 17:49:47 +0900

    rename: 매크로 | 버큰 크기 수정
    Former-commit-id: 046d1fcb98be08c3f58e5ab604fd623b66e1a4bb

--- AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/macro/MacroCafeFragment.kt
+++ AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/macro/MacroCafeFragment.kt
@@ -23,7 +23,7 @@ class MacroCafeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
     }
 
 }
