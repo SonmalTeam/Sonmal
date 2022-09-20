5e51368cb8027bf990d0768a24021231672c7af9
편예린 <dpfls3433@gmail.com>
2022-09-20 14:55:41 +0900

    feat: DB설계 | DB세팅 [S07P22D202-80]
    Former-commit-id: 021084fedb259a259640375a20361d4a4d8252b9

--- Backend/assemble_Server/src/main/java/com/d202/assemble/AssembleServerApplication.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/AssembleServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssembleServerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("build test01");
 		SpringApplication.run(AssembleServerApplication.class, args);
 	}
 
