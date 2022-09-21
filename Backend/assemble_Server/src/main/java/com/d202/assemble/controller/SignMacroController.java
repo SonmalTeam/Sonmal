09af0b481e63238a519fcfed4f9cb03a1cee46a5
Sihyeon <qotlgus123@naver.com>
2022-09-21 17:32:09 +0900

    feat: file | videofile upload test 2 [S07P22D202-334]
    Former-commit-id: ef9a4367c9c6e166869810278af4f513287c8542

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/SignMacroController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/SignMacroController.java
@@ -29,7 +29,7 @@ public class SignMacroController {
     // 매크로 등록
     @ApiOperation(value = "매크로 등록")
     @PostMapping
-    public void createSignMacro(@RequestBody final SignMacroRequestDto request, @RequestParam("file") MultipartFile files){
+    public void createSignMacro(@RequestParam("file") MultipartFile files, SignMacroRequestDto request){
         try {
             String origFilename = files.getOriginalFilename();
             String filename = new MD5Generator(origFilename).toString();
