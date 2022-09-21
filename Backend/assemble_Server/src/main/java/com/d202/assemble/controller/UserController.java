89e1b7a6750eb8111f4e03a2c9c52f81d2ef60b8
편예린 <dpfls3433@gmail.com>
2022-09-21 17:40:17 +0900

    feat: 회원관리 API | 카카오 회원가입 테스트코드 [S07P22D202-343]
    Former-commit-id: 17dfeb590a32084bcfeffe32d6f98dab35e7f522

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/UserController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/UserController.java
@@ -16,19 +16,21 @@ import com.d202.assemble.service.UserService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 
 @Api(value="User")
 @RestController
 @RequestMapping("/user")
+//lombok 생성자 주입 => 초기화되지않은 final 변수에 주입시킴 (생성자가 1개일때 가능)
+@RequiredArgsConstructor
 public class UserController {
 	
-	@Autowired
-	private UserService userService;
+	private final UserService userService;
 	
 	@ApiOperation(value="네이버 회원가입")
 	@PostMapping("/naver/join")
 	public ResponseEntity<?> naverJoin(@RequestBody String token){
-		Map<String, Object> userInfo = userService.getUserInfo(token);
+		Map<String, Object> userInfo = userService.getNaverUserInfo(token);
 		if(userInfo!=null) {
 			User user = new User();
 			user.setEmail(userInfo.get("email").toString());
@@ -39,10 +41,26 @@ public class UserController {
 		return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
 	}
 	
+	@ApiOperation(value="카카오 회원가입")
+	@PostMapping("/kakao/join")
+	public ResponseEntity<?> kakaoJoin(@RequestBody String token){
+		Map<String, Object> userInfo = userService.getNaverUserInfo(token);
+		if(userInfo!=null) {
+			User user = new User();
+			user.setEmail(userInfo.get("email").toString());
+			//test
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+//			if(userService.insertUser(user)) {
+//				return new ResponseEntity<Void>(HttpStatus.OK);
+//			}
+		}
+		return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
+	}
+	
 	@ApiOperation(value="네이버 로그인")
 	@PostMapping("/naver/login")
 	public ResponseEntity<?> naverLogin(@RequestBody String token){
-		Map<String, Object> userInfo = userService.getUserInfo(token);
+		Map<String, Object> userInfo = userService.getNaverUserInfo(token);
 		if(userInfo!=null) {
 			Optional<User> res = userService.getUser((String)userInfo.get("email"));
 			if(res.isPresent()) {
--- Backend/assemble_Server/src/main/java/com/d202/assemble/service/UserService.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/service/UserService.java
@@ -1,12 +1,79 @@
 package com.d202.assemble.service;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import com.d202.assemble.dto.User;
+import javax.transaction.Transactional;
 
-public interface UserService {
-	public boolean insertUser(User user);
-	public Map<String, Object> getUserInfo(String token);
-	public Optional<User> getUser(String email);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.d202.assemble.dto.User;
+import com.d202.assemble.repo.UserRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Transactional
+	public boolean insertUser(User user) {
+		User result = userRepo.save(user);
+		return (result!=null);
+	}
+
+	public Optional<User> getUser(String email) {
+		return userRepo.findByEmail(email);
+	}
+	
+	//naver 회원가입
+	public Map<String, Object> getNaverUserInfo(String token) {
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer "+token);
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		HttpEntity<MultiValueMap<String, String>> header = new HttpEntity<>(headers);
+		ResponseEntity<String> res = rt.exchange(
+				"https://openapi.naver.com/v1/nid/me",
+				HttpMethod.GET,
+				header,
+				String.class
+				);
+		System.out.println(res.getBody());
+		
+		return null;
+	}
+
+	//kakao 회원가입
+	public Map<String, Object> getKakaoUserInfo(String token) {
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer "+token);
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		HttpEntity<MultiValueMap<String, String>> header = new HttpEntity<>(headers);
+		ResponseEntity<String> res = rt.exchange(
+				"https://kapi.kakao.com/v2/user/me",
+				HttpMethod.GET,
+				header,
+				String.class
+				);
+		
+		Map<String, Object> test = new HashMap<>();
+		test.put("email", res.getBody());
+		
+		return test;
+	}
 }
