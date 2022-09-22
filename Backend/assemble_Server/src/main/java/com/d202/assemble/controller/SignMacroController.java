1a2094914c6a4f69f1252aef3bdb6d7fadd469a9
qotlgus123 <qotlgus123@naver.com>
2022-09-22 14:26:53 +0900

    test: macro | 영상 스트리밍 test [S07P22D202-334]
    Former-commit-id: 988b8f122d52a23b7409ad22688a8df685d24422

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/SignMacroController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/SignMacroController.java
@@ -13,12 +13,21 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 @RestController
@@ -102,5 +111,38 @@ public class SignMacroController {
     @PutMapping("/{signMacroSeq}")
     public void updateSignMacro(@PathVariable Long signMacroSeq, @RequestParam Long categorySeq) {
         signMacroService.updateSignMacro(signMacroSeq, categorySeq);
+    }
+
+    @RequestMapping(value = "/api/region", method = RequestMethod.GET)
+    public ResponseEntity<ResourceRegion> videoRegion(@RequestHeader HttpHeaders headers) throws Exception {
+        String path = "/files" + "/0412c29576c708cf0155e8de242169b1.jpg";
+        Resource resource = new FileSystemResource(path);
+
+        long chunkSize = 1024 * 1024;
+        long contentLength = resource.contentLength();
+
+        ResourceRegion region;
+
+        try {
+            HttpRange httpRange = headers.getRange().stream().findFirst().get();
+            long start = httpRange.getRangeStart(contentLength);
+            long end = httpRange.getRangeEnd(contentLength);
+            long rangeLength = Long.min(chunkSize, end -start + 1);
+
+            log.info("start === {} , end == {}", start, end);
+
+            region = new ResourceRegion(resource, start, rangeLength);
+        } catch (Exception e) {
+            long rangeLength = Long.min(chunkSize, contentLength);
+            region = new ResourceRegion(resource, 0, rangeLength);
+        }
+
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+                .contentType(MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .header("Accept-Ranges", "bytes")
+                .eTag(path)
+                .body(region);
+
     }
 }
