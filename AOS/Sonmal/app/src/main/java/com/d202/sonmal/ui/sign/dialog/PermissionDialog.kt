2df024e1499f1a673945ea98362bee57b350369f
nhee0410 <nhee0410@pusan.ac.kr>
2022-09-29 12:01:37 +0900

    feat: 접근 권한 | 접근 권한 다이얼로그 사이즈 조절 [S07P22D202-430]
    Former-commit-id: e40d315122b38d71e3c1a819b7d45cb9a60cf7e4

--- AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/sign/dialog/PermissionDialog.kt
+++ AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/sign/dialog/PermissionDialog.kt
@@ -1,10 +1,14 @@
 package com.d202.sonmal.ui.sign.dialog
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.d202.sonmal.databinding.DialogPermissionBinding
 
@@ -23,6 +27,24 @@ class PermissionDialog(var mContext: Context): DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        resizeDialog()
+    }
+
+    private fun resizeDialog() {
+        val windowManager = mContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        val deviceHeight = size.y
+        params?.width = (deviceWidth * 0.95).toInt()
+        params?.height = (deviceHeight * 0.85).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
