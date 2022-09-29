c273e9a9b18498db4639952881f7ed88dee9ec35
qotlgus123 <qotlgus123@naver.com>
2022-09-29 11:07:29 +0900

    feat: macro | macro sort list pageable [S07P22D202-422]
    Former-commit-id: 3a596b3dbc04f271e833db542f493baa92863604

--- Backend/assemble_Server/src/main/java/com/d202/assemble/config/WebConfig.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private String connectPath = "/sign/macro/**";
-    private String resourcePath = "file:///home/ubuntu/files";
+    private String connectPath = "**/video/**";
+    private String resourcePath = "file://home/ubuntu/files/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
