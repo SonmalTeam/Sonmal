a6404066ad1d84ceaeed34d38640f59b94051ba7
qotlgus123 <qotlgus123@naver.com>
2022-09-30 22:16:11 +0900

    fix: filename fix
    Former-commit-id: 3d1c04608d82642853ab09a344f9edf2194ff14d

--- Backend/assemble_Server/src/main/java/com/d202/assemble/repo/VideoFileRepo.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/repo/VideoFileRepo.java
@@ -3,5 +3,8 @@ package com.d202.assemble.repo;
 import com.d202.assemble.dto.VideoFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VideoFileRepo extends JpaRepository<VideoFile, Long> {
+    Optional<VideoFile> findById(Long id);
 }
