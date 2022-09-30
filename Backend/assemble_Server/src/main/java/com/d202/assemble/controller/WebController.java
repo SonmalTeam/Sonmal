261585cceb1de142acb85c540fdbac3cdc1495c0
편예린 <dpfls3433@gmail.com>
2022-09-30 23:02:36 +0900

    feat: policy 파일 생성 | 파일 완성 [S07P22D202-448]
    Former-commit-id: 367e386a2d9b1a4d2562bd9fc0f943ad680b7261

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/WebController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/WebController.java
@@ -2,8 +2,6 @@ package com.d202.assemble.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.log4j.Log4j2;
 
