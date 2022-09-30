de68d59a1bc8927506b034d20799e45f544d872d
편예린 <dpfls3433@gmail.com>
2022-09-30 22:14:25 +0900

    feat: policy 파일 생성 | 파일 완성 [S07P22D202-448]
    Former-commit-id: d460c5099486f8cae2e5c7b6341392b4cc1f8f96

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/MainController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/MainController.java
@@ -2,12 +2,18 @@ package com.d202.assemble.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j2;
 
 @Controller
+@Log4j2
 public class MainController {
 	
-	@GetMapping("/")
+	@RequestMapping(value="/")
 	public String main() {
-		return "main";
+		log.info("policy출력해라좀");
+		return "policy";
 	}
 }
