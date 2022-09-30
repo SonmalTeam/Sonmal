9c963bac01e51a47fda283a34ecf94c6f4ce9bae
nhee0410 <nhee0410@pusan.ac.kr>
2022-10-01 02:06:02 +0900

    feat: 설정 | 앱 정보 화면 뒤로 가기 버튼 추가 [S07P22D202-449]
    Former-commit-id: 8209f54b0c0702397012d24c747f24db25130719

--- AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/setting/AppInfoFragment.kt
+++ AOS/Sonmal/app/src/main/java/com/d202/sonmal/ui/setting/AppInfoFragment.kt
@@ -5,20 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.d202.sonmal.R
+import com.d202.sonmal.databinding.FragmentAppInfoBinding
 
 
 class AppInfoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentAppInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_info, container, false)
+        binding = FragmentAppInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnBack.setOnClickListener { findNavController().popBackStack() }
+
+            // TODO : 앱 정보 뭐 추가할지 생각해보고 추가하세요...
+        }
     }
 }
