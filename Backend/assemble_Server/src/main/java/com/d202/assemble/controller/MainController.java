0af3b9d0eba844334f84964713f065bce529b370
편예린 <dpfls3433@gmail.com>
2022-09-30 22:29:43 +0900

    feat: policy 파일 생성 | 파일 완성 [S07P22D202-448]
    Former-commit-id: 2163d1d4bd2fb272c3e806007f951606af3247c2

--- Backend/assemble_Server/src/main/java/com/d202/assemble/controller/MainController.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/controller/MainController.java
@@ -11,7 +11,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class MainController {
 	
-	@RequestMapping(value="/")
+	@GetMapping(value="")
 	public String main() {
 		log.info("policy출력해라좀");
 		return "policy";
