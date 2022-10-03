ab8389b54ff40da216b9a60e42762a3f77d3ac34
편예린 <dpfls3433@gmail.com>
2022-10-03 14:34:24 +0900

    fix: JWT refresh token 발급 | 만료시간 테스트용으로 변경 [S07P22D202-367]
    Former-commit-id: 567e2971f3289f0440a8e88475343530f4a45943

--- Backend/assemble_Server/src/main/java/com/d202/assemble/jwt/JwtProperties.java
+++ Backend/assemble_Server/src/main/java/com/d202/assemble/jwt/JwtProperties.java
@@ -4,8 +4,8 @@ package com.d202.assemble.jwt;
 //public static final => interface 사용
 public interface JwtProperties {
 	//String SECRET ="{}";//secret key는 random을 쓸거임
-	int ACCESS_EXP_TIME = 1800000; //만료시간 30분
-//	int ACCESS_EXP_TIME = 30000;//테스트 30초
+//	int ACCESS_EXP_TIME = 1800000; //만료시간 30분
+	int ACCESS_EXP_TIME = 30000;//테스트 30초
 	int REFRESH_EXP_TIME = 1_209_600_000; //만료시간 2주
 	//String TOKEN_PREFIX = "Bearer ";//토큰 앞에 붙이는 것, 전달자명(없어도 되는듯)
 	String JWT_ACCESS_NAME = "JWT-AUTHENTICATION";
